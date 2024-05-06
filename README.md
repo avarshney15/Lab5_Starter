26a42a2bfd295845283a67976e98e0e88f21d534
David Wang <davidwang334@gmail.com>
2024-05-05 18:45:35 -0700

    unit testing

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Arjun Varshney
 David Wang
 
+
+Part 3:
+1. No, I would not use unit tests to test the "message" feature. This is because ensuring that a message one user sent is received by another user involves multiple parts of the application such as the server, the client, and the network connection between the two users. It has too many moving parts to unit test.
+2. Yes, I would use a unit test to test the "max message length" feature of a messaging application. This constraint is something simple that can be easily isolated and tested within a unit test without a dependency on other components. The unit test can verify that the input message is truncated to 80 characters.
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,101 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+/*
+isPhoneNumber
+*/
+test("isPhoneNumber: 123-456-7890", () => {
+  expect(isPhoneNumber("123-456-7890")).toBe(true);
+});
+
+test("isPhoneNumber: (123)456-7890", () => {
+  expect(isPhoneNumber("(123)456-7890")).toBe(true);
+});
+
+test("isPhoneNumber: 1234567890", () => {
+  expect(isPhoneNumber("1234567890")).toBe(false);
+});
+
+test("isPhoneNumber: 9", () => {
+  expect(isPhoneNumber("9")).toBe(false);
+});
+
+/*
+isEmail
+*/
+test("isEmail: tpowell2@ucsd.edu", () => {
+  expect(isEmail("tpowell2@ucsd.edu")).toBe(true);
+});
+
+test("isEmail: ken@zeger.us", () => {
+  expect(isEmail("ken@zeger.us")).toBe(true);
+});
+
+test("isEmail: ken@zeger", () => {
+  expect(isEmail("ken@zeger")).toBe(false);
+});
+
+test("isEmail: 123442as", () => {
+  expect(isEmail("123442as")).toBe(false);
+});
+
+/*
+isStrongPassword
+*/
+test("isStrongPassword: Password", () => {
+  expect(isStrongPassword("Password")).toBe(true);
+});
+
+test("isStrongPassword: davidwang_22", () => {
+  expect(isStrongPassword("davidwang_22")).toBe(true);
+});
+
+test("isStrongPassword: dav", () => {
+  expect(isStrongPassword("dav")).toBe(false);
+});
+
+test("isStrongPassword: 1234234213421341234", () => {
+  expect(isStrongPassword("1234234213421341234")).toBe(false);
+});
+
+/*
+isDate
+*/
+test("isDate: 12/12/2020", () => {
+  expect(isDate("12/12/2020")).toBe(true);
+});
+
+test("isDate: 1/1/2025", () => {
+  expect(isDate("1/1/2025")).toBe(true);
+});
+
+test("isDate: 11-2025", () => {
+  expect(isDate("11-2025")).toBe(false);
+});
+
+test("isDate: 11/11/2025234", () => {
+  expect(isDate("11/11/2025234")).toBe(false);
+});
+
+/*
+isHexColor
+*/
+
+test("isHexColor: #123456", () => {
+  expect(isHexColor("#123456")).toBe(true);
+});
+
+
+test("isHexColor: #FFF", () => {
+  expect(isHexColor("#FFF")).toBe(true);
+});
+
+test("isHexColor: #ZZZ", () => {
+  expect(isHexColor("#ZZZ")).toBe(false);
+});
+
+test("isHexColor: #1234567", () => {
+  expect(isHexColor("#1234567")).toBe(false);
+});
+
+
